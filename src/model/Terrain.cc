e5e2a1de460d4c6f5356479b2a27b0f124ace0af
Eric <ericekstrm@gmail.com>
2020-11-05 17:04:10 +0100

    Started working on random noise terrain. Not done yet

--- src/model/Terrain.cc
+++ src/model/Terrain.cc
@@ -20,15 +20,37 @@ void Terrain::generate_terrain()
 {
     //heightmap_terrain("island3.png");
     flat_terrain();
-    
+    //generate_perlin_terrain();
+
     set_indices();
+}
+
+void Terrain::set_indices()
+{
+    for (int i = 0; i < terrain_resolution - 1; i++)
+    {
+        for (int j = 0; j < terrain_resolution - 1; j++)
+        {
+            int topLeft = i * (terrain_resolution) +j;
+            int topRight = topLeft + 1;
+            int bottomLeft = (i + 1) * (terrain_resolution) +j;
+            int bottomRight = bottomLeft + 1;
+
+            indices.push_back(topRight);
+            indices.push_back(bottomLeft);
+            indices.push_back(topLeft);
+            indices.push_back(bottomRight);
+            indices.push_back(bottomLeft);
+            indices.push_back(topRight);
+        }
+    }
 }
 
 void Terrain::flat_terrain()
 {
-    for (int i = -terrain_resolution / 2; i < terrain_resolution / 2 + 1; i++)
+    for (int i = -terrain_resolution / 2; i < terrain_resolution / 2; i++)
     {
-        for (int j = -terrain_resolution / 2; j < terrain_resolution / 2 + 1; j++)
+        for (int j = -terrain_resolution / 2; j < terrain_resolution / 2; j++)
         {
             float r = 0;
             vertices.push_back(terrain_size / terrain_resolution * i);
@@ -101,23 +123,76 @@ void Terrain::heightmap_terrain(std::string const& file_name)
     }
 }
 
-void Terrain::set_indices()
+void Terrain::generate_perlin_terrain()
 {
-    for (int i = 0; i < terrain_resolution - 1; i++)
-    {
-        for (int j = 0; j < terrain_resolution - 1; j++)
-        {
-            int topLeft = i * (terrain_resolution) +j;
-            int topRight = topLeft + 1;
-            int bottomLeft = (i + 1) * (terrain_resolution) +j;
-            int bottomRight = bottomLeft + 1;
+    int num_octaves;    //number of noise functions of different density, more octaves means higher level of detail
+    float smoothness;   //between 0-1
+    int seed;           //for the random part, (same seed=same output)
 
-            indices.push_back(topRight);
-            indices.push_back(bottomLeft);
-            indices.push_back(topLeft);
-            indices.push_back(bottomRight);
-            indices.push_back(bottomLeft);
-            indices.push_back(topRight);
+
+    for (int i = -terrain_resolution / 2; i < terrain_resolution / 2; i++)
+    {
+        for (int j = -terrain_resolution / 2; j < terrain_resolution / 2; j++)
+        {
+            float height {get_perlin_height(i, j)};
+            vertices.push_back(terrain_size / terrain_resolution * i);
+            vertices.push_back(height);
+            vertices.push_back(terrain_size / terrain_resolution * j);
+
+            normals.push_back(0);
+            normals.push_back(1);
+            normals.push_back(0);
+
+            texture_coords.push_back(static_cast<float>(i) / terrain_resolution * 20);
+            texture_coords.push_back(static_cast<float>(j) / terrain_resolution * 20);
         }
     }
+}
+
+float Terrain::get_perlin_height(int x, int z) const
+{
+    float total_height {0};
+    float frequency {1.0};
+    float smoothness {0.5};
+
+    for (int i = 0; i < 1; i++)
+    {
+        float amplitude {pow((1 - smoothness), i)};
+
+        total_height += amplitude * generate_interpolated_height(x * frequency, z * frequency);
+
+        frequency *= 2;
+    }
+    
+    return total_height;
+}
+
+float Terrain::generate_perlin_noise(int x, int z) const
+{
+    srand(x + z * terrain_size);
+    return ((float) rand() / (float) RAND_MAX);
+}
+
+float Terrain::generate_interpolated_height(float x, float z) const
+{
+    int int_x = static_cast<int>(x);
+    int int_z = static_cast<int>(z);
+    float fraction_x = x - int_x;
+    float fraction_z = z - int_z;
+
+    float ul = generate_perlin_noise(int_x,     int_z);
+    float ur = generate_perlin_noise(int_x + 1, int_z);
+    float dl = generate_perlin_noise(int_x,     int_z + 1);
+    float dr = generate_perlin_noise(int_x + 1, int_z + 1);
+
+    float h1 = interpolate(ul, ur, fraction_x);
+    float h2 = interpolate(dl, dr, fraction_x);
+
+    return interpolate(h1, h2, fraction_z);
+}
+
+float Terrain::interpolate(float p1, float p2, float blend) const
+{
+    float new_blend = (1.0 - cos(blend * 3.1415)) * 0.5; //value between 0 and 1
+    return p1 * (1 - new_blend) + p2 * new_blend; 
 }
--- src/model/Terrain.h
+++ src/model/Terrain.h
@@ -12,6 +12,7 @@ private:
 
     // Terrain_size is the size that the texture "tile" will take up in the world.
     float terrain_size = 128;
+    
     // Terrain_resolution is the number of vertices in each dimension of the texture "tile".
     // Also then the size in each dimension of the heightmap image file.
     int terrain_resolution = 512;
@@ -19,10 +20,18 @@ private:
     float max_height {20};
 
     void generate_terrain();
+    void set_indices();
+
+    //terrain generation functions
     void flat_terrain();
     void heightmap_terrain(std::string const& file_name);
+    void generate_perlin_terrain();
 
-    void set_indices();
+    //perlin noise functions
+    float get_perlin_height(int x, int z) const;
+    float generate_perlin_noise(int x, int z) const;
+    float generate_interpolated_height(float x, float z) const;
+    float interpolate(float p1, float p2, float blend) const;
 
     std::vector<float> vertices = {};
     std::vector<float> normals {};
